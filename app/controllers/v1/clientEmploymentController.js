e0c7f3a35d7c057c97f90e24c81c04706f5cae1d
developer-talaghay <developer@talaghay.com>
2023-06-21 03:39:40 +0800

    created endpoints for employment

--- app/controllers/v1/clientEmploymentController.js
+++ app/controllers/v1/clientEmploymentController.js
@@ -1,11 +1,79 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/v1/userController');
+const ClientEmployment = require("../../models/clientEmployment");
 
-router.post('/', userController.createUser);
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUser);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+const clientEmploymentController = {
+    createClientEmployment: (req, res) => {
+      const { client_id, company_name, income, status, years, address, contact_number, head } =
+        req.body;
+  
+      const newClientEmployment = new ClientEmployment(
+        null,
+        client_id, 
+        company_name, 
+        income, status, 
+        years, address, 
+        contact_number, 
+        head 
+      );
+  
+      ClientEmployment.createClientEmployment(newClientEmployment)
+        .then((clientEmploymentId) => {
+          res
+            .status(201)
+            .json({ id: clientEmploymentId, message: "Client Employment created successfully" });
+        })
+        .catch((err) => {
+          console.log(err); // Add this line to print the error.
+          res.status(500).json({ error: "Failed to create client Employment" });
+        });
+    },  
 
-module.exports = router;
+    updateClientEmployment: (req, res) => {
+        const clientEmploymentId = parseInt(req.query.id);
+        const { client_id, company_name, income, status, years, address, contact_number, head } = req.body;
+      
+        const updatedClientEmployment = new ClientEmployment(
+          clientEmploymentId,
+          client_id, 
+        company_name, 
+        income, 
+        status, 
+        years, 
+        address, 
+        contact_number, 
+        head 
+        );
+      
+        ClientEmployment.updateClientEmployment(updatedClientEmployment)
+          .then((success) => {
+            console.log("Update Success:", success);
+            if (success) {
+              res.json({ message: "Client Employment updated successfully" });
+            } else {
+              res.status(404).json({ error: "Client Employment not found" });
+            }
+          })
+          .catch((err) => {
+            console.error("Update Error:", err);
+            res.status(500).json({ error: "Failed to update client Employment" });
+          });
+      },
+      
+      
+      deleteClientEmployment: (req, res) => {
+        const clientEmploymentId = parseInt(req.query.id);
+    
+        ClientEmployment.deleteClientEmployment(clientEmploymentId)
+          .then((success) => {
+            if (success) {
+              res.json({ message: "Client Employment deleted successfully" });
+            } else {
+              res.status(404).json({ error: "Client Employment not found" });
+            }
+          })
+          .catch((err) => {
+            res.status(500).json({ error: "Failed to delete client Employment" });
+          });
+      },
+}
+
+module.exports = clientEmploymentController;
--- app/models/clientEmployment.js
+++ app/models/clientEmployment.js
@@ -1,11 +1,105 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/v1/userController');
+const { getConnection } = require("../db");
 
-router.post('/', userController.createUser);
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUser);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+class ClientEmployment {
+    constructor(id, client_id, company_name, income, status, years, address, contact_number, head) {
+      this.id = id;
+      this.client_id = client_id;
+      this.company_name = company_name;
+    this.income = income;
+    this.status = status;
+    this.years = years;
+    this.address = address;
+    this.contact_number = contact_number;
+    this.head = head;
+    }
+  
+    static async createClientEmployment(clientEmployment) {
+  
+      const session = await getConnection();
+  
+      if (!session) {
+        throw new Error("Failed to establish a database session");
+      }
+  
+      const sqlQuery = `INSERT INTO client_employment (client_id, company_name, income, status, years, address, contact_number, head)
+                        VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+      const result = await session
+        .sql(sqlQuery)
+        .bind(
+          clientEmployment.client_id,
+          clientEmployment.company_name,
+          clientEmployment.income,
+          clientEmployment.status,
+          clientEmployment.years,
+          clientEmployment.address,
+          clientEmployment.contact_number,
+          clientEmployment.head
+        
+        )
+        .execute();
+  
+      return result.getAutoIncrementValue();
+    }
 
-module.exports = router;
+    static async updateClientEmployment(updatedClientEmployment) {
+        const session = await getConnection();
+      
+        if (!session) {
+          throw new Error("Failed to establish a database session");
+        }
+      
+        const sqlQuery = `UPDATE client_employment
+                          SET client_id = ?,
+                              company_name = ?,
+                              income = ?,
+                              status = ?,
+                              years = ?,
+                              address = ?,
+                              contact_number = ?,
+                              head = ?
+                          WHERE id = ?`;
+      
+        console.log("SQL Query:", sqlQuery);
+        console.log("Updated Client Employment:", updatedClientEmployment);
+      
+        const result = await session
+          .sql(sqlQuery)
+          .bind(
+            updatedClientEmployment.client_id,
+            updatedClientEmployment.company_name,
+            updatedClientEmployment.income,
+            updatedClientEmployment.status,
+            updatedClientEmployment.years,
+            updatedClientEmployment.address,
+            updatedClientEmployment.contact_number,
+            updatedClientEmployment.head,
+            updatedClientEmployment.id
+          )
+          .execute();
+      
+        console.log("Execution Result:", result);
+      
+        return result.getAffectedItemsCount() > 0;
+      }
+        
+      
+      
+      static async deleteClientEmployment(clientEmploymentId) {
+        const session = await getConnection();
+    
+        if (!session) {
+          throw new Error("Failed to establish a database session");
+        }
+    
+        const sqlQuery = `DELETE FROM client_employment WHERE id = ?`;
+        const result = await session
+          .sql(sqlQuery)
+          .bind(clientEmploymentId)
+          .execute();
+    
+        return result.getAffectedItemsCount() > 0;
+      }
+  }
+  
+
+  module.exports = ClientEmployment;

4868c534be5ef3ae8fbba81dd06ce49eb4e52862
developer-talaghay <developer@talaghay.com>
2023-07-11 04:38:33 +0800

    audit trail for branches

--- app/models/branch.js
+++ app/models/branch.js
@@ -12,13 +12,21 @@ class Branch {
     this.country = country;
   }
 
-  static async createBranch(branch) {
+  static async createBranch(branch, user_id) {
     const session = await getConnection();
-
+  
     if (!session) {
       throw new Error("Failed to establish a database session");
     }
-
+  
+    const auditTrailQuery = `INSERT INTO audit_trail (user_id, action_type)
+                             VALUES (?, ?)`;
+  
+    await session
+      .sql(auditTrailQuery)
+      .bind(user_id, 6) // Set the user_id from req.body and action_type to 6
+      .execute();
+  
     const sqlQuery = `INSERT INTO branch (name, address_line1, address_line2, city, province, postal_code, country)
                       VALUES (?, ?, ?, ?, ?, ?, ?)`;
     const result = await session
@@ -33,44 +41,90 @@ class Branch {
         branch.country
       )
       .execute();
-
+  
     return result.getAutoIncrementValue();
   }
+  
 
-  static async getAllBranch() {
+  static async getAllBranch(user_id) {
     const session = await getConnection();
-
+  
     if (!session) {
       throw new Error("Failed to establish a database session");
     }
-
+  
+    const auditTrailQuery = `INSERT INTO audit_trail (user_id, action_type)
+                             VALUES (?, ?)`;
+  
+    await session
+      .sql(auditTrailQuery)
+      .bind(user_id, 7) // Set the user_id from req.body and action_type to 7
+      .execute();
+  
     const sqlQuery = "SELECT * FROM branch";
     const result = await session.sql(sqlQuery).execute();
-
-    return result.fetchAll();
+  
+    const branches = result.fetchAll();
+    return branches.map((branch) => ({
+      id: branch[0],
+      name: branch[1],
+      address_line1: branch[2],
+      address_line2: branch[3],
+      city: branch[4],
+      province: branch[5],
+      postal_code: branch[6],
+      country: branch[7],
+    }));
   }
+  
 
-  static async getBranchById(branchId) {
+  static async getBranchById(branchId, user_id) {
     const session = await getConnection();
-
+  
     if (!session) {
       throw new Error("Failed to establish a database session");
     }
-
+  
+    const auditTrailQuery = `INSERT INTO audit_trail (user_id, action_type)
+                             VALUES (?, ?)`;
+  
+    await session
+      .sql(auditTrailQuery)
+      .bind(user_id, 8) // Set the user_id from req.body and action_type to 8
+      .execute();
+  
     const sqlQuery = `SELECT * FROM branch WHERE id = ?`;
     const result = await session.sql(sqlQuery).bind(branchId).execute();
-
+  
     const branches = result.fetchAll();
-    return branches.length ? branches[0] : null;
+    return branches.map((branch) => ({
+      id: branch[0],
+      name: branch[1],
+      address_line1: branch[2],
+      address_line2: branch[3],
+      city: branch[4],
+      province: branch[5],
+      postal_code: branch[6],
+      country: branch[7],
+    }));
   }
+  
 
-  static async updateBranch(branch) {
+  static async updateBranch(branch, user_id) {
     const session = await getConnection();
-
+  
     if (!session) {
       throw new Error("Failed to establish a database session");
     }
-
+  
+    const auditTrailQuery = `INSERT INTO audit_trail (user_id, action_type)
+                             VALUES (?, ?)`;
+  
+    await session
+      .sql(auditTrailQuery)
+      .bind(user_id, 9) // Set the user_id from req.body and action_type to 9
+      .execute();
+  
     const sqlQuery = `UPDATE branch SET name = ?, address_line1 = ?, address_line2 = ?, city = ?, province = ?, postal_code = ?, country = ? WHERE id = ?`;
     const result = await session
       .sql(sqlQuery)
@@ -85,23 +139,32 @@ class Branch {
         branch.id
       )
       .execute();
-
+  
     return result.getAffectedItemsCount() > 0;
   }
+  
 
-  static async deleteBranch(branchId) {
+  static async deleteBranch(branchId, user_id) {
     const session = await getConnection();
   
     if (!session) {
       throw new Error("Failed to establish a database session");
     }
   
+    const auditTrailQuery = `INSERT INTO audit_trail (user_id, action_type)
+                             VALUES (?, ?)`;
+  
+    await session
+      .sql(auditTrailQuery)
+      .bind(user_id, 10) // Set the user_id from req.body and action_type to 9
+      .execute();
+  
     const sqlQuery = `DELETE FROM branch WHERE id = ?`;
     const result = await session.sql(sqlQuery).bind(branchId).execute();
   
-    await session.close();
     return result.getAffectedItemsCount() > 0;
-    }
+  }
+  
 }
 
 module.exports = Branch;

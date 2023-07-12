27e3bc7c27785bda8778dc7b7a92239e015f51ad
developer-talaghay <developer@talaghay.com>
2023-07-12 19:04:32 +0800

    updated the client address for audit trail

--- app/models/clientAddress.js
+++ app/models/clientAddress.js
@@ -13,12 +13,20 @@ class ClientAddress {
     this.country = country;
   }
 
-  static async createClientAddress(clientAddress) {
+  static async createClientAddress(clientAddress, userId) {
     const session = await getConnection();
 
     if (!session) {
       throw new Error("Failed to establish a database session");
     }
+
+    const auditTrailQuery = `INSERT INTO audit_trail (user_id, action_type)
+                           VALUES (?, ?)`;
+
+    await session
+      .sql(auditTrailQuery)
+      .bind(userId, 16) // Set the user_id from req.body and action_type to 16
+      .execute();
 
     const sqlQuery = `INSERT INTO client_address (client_id, address_type, address_line1, address_line2, city, province, postal_code, country)
                       VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
@@ -39,13 +47,22 @@ class ClientAddress {
     return result.getAutoIncrementValue();
   }
 
-  static async updateClientAddress(clientAddress) {
+  
+  static async updateClientAddress(clientAddress, userId) {
     const session = await getConnection();
-
+  
     if (!session) {
       throw new Error("Failed to establish a database session");
     }
-
+  
+    const auditTrailQuery = `INSERT INTO audit_trail (user_id, action_type)
+                             VALUES (?, ?)`;
+  
+    await session
+      .sql(auditTrailQuery)
+      .bind(userId, 17) // Set the user_id from req.body and action_type to 17
+      .execute();
+  
     const sqlQuery = `UPDATE client_address SET client_id = ?, address_type = ?, address_line1 = ?, address_line2 = ?, city = ?, province = ?, postal_code = ?, country = ? WHERE id = ?`;
     const result = await session
       .sql(sqlQuery)
@@ -61,25 +78,35 @@ class ClientAddress {
         clientAddress.id
       )
       .execute();
-
+  
     return result.getAffectedItemsCount() > 0;
   }
+  
 
-  static async deleteClientAddress(clientAddressId) {
+  static async deleteClientAddress(clientAddressId, userId) {
     const session = await getConnection();
-
+  
     if (!session) {
       throw new Error("Failed to establish a database session");
     }
-
+  
+    const auditTrailQuery = `INSERT INTO audit_trail (user_id, action_type)
+                             VALUES (?, ?)`;
+  
+    await session
+      .sql(auditTrailQuery)
+      .bind(userId, 18) // Set the user_id from req.body and action_type to 18
+      .execute();
+  
     const sqlQuery = `DELETE FROM client_address WHERE id = ?`;
     const result = await session
       .sql(sqlQuery)
       .bind(clientAddressId)
       .execute();
-
+  
     return result.getAffectedItemsCount() > 0;
   }
+  
 }
 
 module.exports = ClientAddress;

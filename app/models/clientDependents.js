d730c316705cbd49e1607801a7ef2509eac53843
developer-talaghay <developer@talaghay.com>
2023-07-12 21:38:32 +0800

    added audit trail for dependents

--- app/models/clientDependents.js
+++ app/models/clientDependents.js
@@ -10,14 +10,13 @@ class ClientDependents {
     this.date_of_birth = date_of_birth;
     }
   
-    static async createClientDependents(clientDependents) {
-  
+    static async createClientDependents(clientDependents, userId) { // Add userId parameter
       const session = await getConnection();
-  
+    
       if (!session) {
         throw new Error("Failed to establish a database session");
       }
-  
+    
       const sqlQuery = `INSERT INTO client_dependents (client_id, first_name, middle_name, last_name, date_of_birth)
                         VALUES (?, ?, ?, ?, ?)`;
       const result = await session
@@ -30,63 +29,82 @@ class ClientDependents {
           clientDependents.date_of_birth
         )
         .execute();
-  
+    
+      // Write to audit_trail table
+      const auditTrailQuery = `INSERT INTO audit_trail (user_id, action_type)
+                               VALUES (?, ?)`;
+      await session
+        .sql(auditTrailQuery)
+        .bind(userId, 25) // Set the user_id from req.body and action_type to 25
+        .execute();
+    
       await session.close();
       return result.getAutoIncrementValue();
-    }
+    }    
 
-    static async updateClientDependents(updatedClientDependents) {
-        const session = await getConnection();
-      
-        if (!session) {
-          throw new Error("Failed to establish a database session");
-        }
-      
-        const sqlQuery = `UPDATE client_dependents
-                          SET client_id = ?,
-                              first_name = ?,
-                              middle_name = ?,
-                              last_name = ?,
-                              date_of_birth = ?
-                          WHERE id = ?`;
-      
-        console.log("SQL Query:", sqlQuery);
-        console.log("Updated Client Dependents:", updatedClientDependents);
-      
-        const result = await session
-          .sql(sqlQuery)
-          .bind(
-            updatedClientDependents.client_id,
-            updatedClientDependents.first_name,
-            updatedClientDependents.middle_name,
-            updatedClientDependents.last_name,
-            updatedClientDependents.date_of_birth,
-            updatedClientDependents.id
-          )
-          .execute();
-      
-        console.log("Execution Result:", result);
-      
-        return result.getAffectedItemsCount() > 0;
+    static async updateClientDependents(updatedClientDependents, userId) {
+      const session = await getConnection();
+    
+      if (!session) {
+        throw new Error("Failed to establish a database session");
       }
-        
+    
+      const sqlQuery = `UPDATE client_dependents
+                        SET client_id = ?,
+                            first_name = ?,
+                            middle_name = ?,
+                            last_name = ?,
+                            date_of_birth = ?
+                        WHERE id = ?`;
+    
+      const result = await session
+        .sql(sqlQuery)
+        .bind(
+          updatedClientDependents.client_id,
+          updatedClientDependents.first_name,
+          updatedClientDependents.middle_name,
+          updatedClientDependents.last_name,
+          updatedClientDependents.date_of_birth,
+          updatedClientDependents.id
+        )
+        .execute();
+    
+      // Write to audit_trail table
+      const auditTrailQuery = `INSERT INTO audit_trail (user_id, action_type)
+                               VALUES (?, ?)`;
+      await session
+        .sql(auditTrailQuery)
+        .bind(userId, 26) // Set the user_id from req.body and action_type to 26
+        .execute();
+    
+      await session.close();
+      return result.getAffectedItemsCount() > 0;
+    }
+    
       
       
-      static async deleteClientDependents(clientDependentsId) {
-        const session = await getConnection();
+    static async deleteClientDependents(clientDependentsId, userId) {
+      const session = await getConnection();
     
-        if (!session) {
-          throw new Error("Failed to establish a database session");
-        }
-    
-        const sqlQuery = `DELETE FROM client_dependents WHERE id = ?`;
-        const result = await session
-          .sql(sqlQuery)
-          .bind(clientDependentsId)
-          .execute();
-    
-        return result.getAffectedItemsCount() > 0;
+      if (!session) {
+        throw new Error("Failed to establish a database session");
       }
+    
+      const auditTrailQuery = `INSERT INTO audit_trail (user_id, action_type)
+                               VALUES (?, ?)`;
+    
+      await session
+        .sql(auditTrailQuery)
+        .bind(userId, 27) // Set the user_id from req.body and action_type to 27
+        .execute();
+    
+      const sqlQuery = `DELETE FROM client_dependents WHERE id = ?`;
+      const result = await session.sql(sqlQuery).bind(clientDependentsId).execute();
+    
+      await session.close();
+      return result.getAffectedItemsCount() > 0;
+    }
+    
   }
   
 
